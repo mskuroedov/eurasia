bc489c0bc460c0f76982e84a94d603dea0db08f3
mskuroedov <mskuroedov@gmail.com>
2020-03-25 22:04:00 +0300

    new animation

--- js/landing.js
+++ js/landing.js
@@ -80,7 +80,7 @@ var animate_timeline = new Animate({
         for (let i = 1; i <= 3; i++) {
             setTimeout(function (e) {
                 $('.timeline-' + i).addClass('js-animated');
-            }, 1000*i)
+            }, 1000*(i-1))
         }
     }
 });
