ad8d781339d6f0e7d847a5149c4b1552cf8e9352
mskuroedov <mskuroedov@gmail.com>
2020-03-27 19:06:08 +0300

    added modals on buttons

--- js/modals.js
+++ js/modals.js
@@ -42,3 +42,7 @@ $('#request_success').on('click', function () {
     $('#request_modal').modal('hide')
     $('#request_success_modal').modal('show')
 });
+$('#callback_success').on('click', function () {
+    $('#callback_modal').modal('hide')
+    $('#request_success_modal').modal('show')
+});
