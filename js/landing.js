7b81a9a3c4d47ed39cb516cdd0e17598ee39b2cb
mskuroedov <mskuroedov@gmail.com>
2020-03-28 14:25:26 +0300

    counting added

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,40 @@
+$(document).ready(function () {
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ' ',
+    };
+    $('.countupthis').each(function () {
+        var num = $(this).attr('numx'); //end count
+        var nuen = $(this).text();
+        if (nuen === "") {
+            nuen = 0;
+        }
+        var counts = new CountUp(this, nuen, num, 0, 1.5, options);
+        counts.start();
+    });
+});
+var animate_car_block = new Animate({
+    target: '.l-desktop',
+    offset: [1, 0.25]
+});
+animate_car_block.init();
+
+var animate_timeline = new Animate({
+    target: '[animate-timeline]',
+    offset: [0.5, 0.5],
+    animatedClass: [''],
+    delay: 0,
+    callbackOnInView: () => {
+        for (let i = 1; i <= 3; i++) {
+            setTimeout(function (e) {
+                $('.timeline-' + i).addClass('js-animated');
+            }, 1000 * (i - 1))
+        }
+    }
+});
+animate_timeline.init();
+
 var specsSlider = $('.specs-slider');
 specsSlider.owlCarousel({
     items: 4,
@@ -64,24 +101,3 @@ newsSlider.owlCarousel({
         }
     }
 })
-
-var animate_car_block = new Animate({
-    target: '.l-desktop',
-    offset: [1, 0.25]
-});
-animate_car_block.init();
-
-var animate_timeline = new Animate({
-    target: '[animate-timeline]',
-    offset: [0.5, 0.5],
-    animatedClass: [''],
-    delay: 0,
-    callbackOnInView: () => {
-        for (let i = 1; i <= 3; i++) {
-            setTimeout(function (e) {
-                $('.timeline-' + i).addClass('js-animated');
-            }, 1000*(i-1))
-        }
-    }
-});
-animate_timeline.init();
