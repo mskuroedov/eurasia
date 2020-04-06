019f626f6c28a36c1394c2d84263ecd70021ad6f
mskuroedov <mskuroedov@gmail.com>
2020-04-06 20:42:23 +0300

    landing fixes + banner

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,23 @@
+//banner
+let banner_slider = $('.l1__banner')
+banner_slider.owlCarousel({
+    margin: 0,
+    items: 1,
+    dots: true,
+    loop:true,
+    navText: [
+        '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 15L10.05 13.95L5.85 9.75L15 9.75L15 8.25L5.85 8.25L10.05 4.05L9 3L3 9L9 15Z" fill="#5F5F5F"/></svg>',
+        '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 3L7.95 4.05L12.15 8.25H3L3 9.75H12.15L7.95 13.95L9 15L15 9L9 3Z" fill="#5F5F5F"/></svg>'],
+    responsive: {
+        0: {
+            nav: false,
+        },
+        1200: {
+            nav: true,
+        }
+    }
+})
+
 $(document).ready(function () {
     var options = {
         useEasing: true,
